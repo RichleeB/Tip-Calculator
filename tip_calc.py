ce238624d1dfe2b7d94a198534a6afc6e3dab507
RichleeB <72957995+RichleeB@users.noreply.github.com>
2020-11-06 06:04:01 -0500

    Create tip_calc.py

---
+++ tip_calc.py
@@ -0,0 +1,14 @@
+print("Please use this tip calculator")
+bill = float(input("What was the total bill? "))
+tip = int(input("How much % would you like to give? Please enter 10 , 12, or 15. Anything less and you are too cheap to need this calculator.. Enter number:  "))
+
+
+tip_calc = bill * tip/100
+
+total_bill = bill + tip_calc
+
+patrons = int(input("Number of people splitting the bill: "))
+
+amount_due = total_bill / patrons 
+
+print(f"Each person should pay {amount_due}")
